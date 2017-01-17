818d98ff7cb36a0ac6875cb0d30151f0e440e3c1
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-17 14:52:24 -0800

    More evil

--- src/app/pages/home/container/Home.js
+++ src/app/pages/home/container/Home.js
@@ -9,7 +9,7 @@ export default class Home extends React.Component {
   render() {
     return(
       <div id="home">
-        <h1>Homepage <Octicon mega name="octoface"/></h1>
+        <h1>EVIL BIKES</h1>
       </div>
     );
   }
