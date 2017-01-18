7eaf934725906d87288522b662bc680d8527367b
Edward Stone <edwardjstone@yahoo.com>
2017-01-18 11:57:28 -0800

    build folder & filename in webpack s3 config

--- webpack.config.s3.js
+++ webpack.config.s3.js
@@ -2,20 +2,22 @@ var webpack = require('webpack');
 var S3Plugin = require('webpack-s3-plugin');
 
 module.exports = require('./shared.webpack.config')({
+  outputFolder: 'build',
+  outputFilename: '[name]',
   plugins: [
     new S3Plugin({
       s3Options: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_DEFAULT_REGION
+        region: 'us-west-2'
       },
       s3UploadOptions: {
-        Bucket: process.env.AWS_S3_BUCKET
+        Bucket: 'evil-cookie-demo'
       },
-      cloudfrontInvalidateOptions: {
-        DistributionId: process.env.AWS_CLOUDFRONT_DISTRIBUTION_ID,
-        Items: ["/*"]
-      }
+      // cloudfrontInvalidateOptions: {
+      //   DistributionId: process.env.AWS_CLOUDFRONT_DISTRIBUTION_ID,
+      //   Items: ["/*"]
+      // }
     })
   ]
 });
