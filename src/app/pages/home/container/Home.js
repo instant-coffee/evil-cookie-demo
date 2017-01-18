58b7fbd9ac73191faf61222109a54c71bd1064e5
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-18 09:40:57 -0800

    Remove commented out

--- src/app/pages/home/container/Home.js
+++ src/app/pages/home/container/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Immutable from 'seamless-immutable';
 import Octicon from 'react-octicon';
 import pureRender from '../../../decorators/pureRender';
+import Cookieconsent from 'cookieconsent';
 
 @pureRender
 export default class Home extends React.Component {
 
   render() {
-    return(
-      <div id="home">
-        <h1>EVIL BIKES</h1>
-      </div>
-    );
+    // return(
+    //   <div id="home">
+    //     <h1 className="text-center">EVIL BIKES</h1>
+    //   </div>
+    // );
   }
 }
