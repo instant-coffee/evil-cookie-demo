48833ba1273d5631707963d23ed9749ecf8dcebc
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-18 10:11:50 -0800

    Adding fake nav

--- src/app/pages/home/container/Home.js
+++ src/app/pages/home/container/Home.js
@@ -10,7 +10,12 @@ export default class Home extends React.Component {
   render() {
     return(
       <div id="home">
-        <h1>EVIL</h1>
+        <h1>EVIL
+        	<span className="faux-nav">Bikes</span>
+        	<span className="faux-nav">Gear</span>
+        	<span className="faux-nav">Tech Crap</span>
+        	<span className="faux-nav">Parts</span>
+        </h1>
       </div>
     );
   }
